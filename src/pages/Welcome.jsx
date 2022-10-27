922e8ecf13ff300d3ae56c9fe69549bc8766eae6
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-10-27 16:10:51 -0300

    added background img and globalCompact component

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import BannerGlobalCompact from '../components/BannerGlobalCompact'
 import NavBar from '../components/NavBar'
 import UI from '../components/UI'
 
 function Welcome() {
   return (
+    <>
+    
     <div className='main-top'>
     <NavBar/>
     <UI/>
+    
     </div>
+    <BannerGlobalCompact/>
+    </>
   )
 }
 
