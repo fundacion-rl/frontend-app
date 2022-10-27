a010deb437662b68b6f03e5d0979316c3cf85e15
Natalia Torres <nataliatorres88@gmail.com>
2022-10-27 11:06:33 -0500

    added hero to welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import UI from '../components/UI'
+import Hero from '../components/Hero'
 
 function Welcome() {
   return (
     <div className='main-top'>
     <NavBar/>
     <UI/>
+    <Hero/>
     </div>
   )
 }
