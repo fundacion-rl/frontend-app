9c44eacd4d21180f4f040389fa584fa4689745a0
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-10-27 16:10:15 -0300

    added globalComponent and styles

---
+++ src/components/BannerGlobalCompact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/GlobalCompact.css'
+
+export default function BannerGlobalCompact() {
+  return (
+    <div className='div-BannerGlobal'>
+        <div className='div-logoGC'>
+          <img className='globalCompact-Logo' src="http://drive.google.com/uc?export=view&id=1ByzxNTI-OdbnH5AJUNi9i767FYyUxI2N" alt="global compact UN" />
+        </div>
+        <div className='div-textGC' >
+            <p>
+            Desde 2020, FUNDACION RETORNO A LA LIBERTAD se ha comprometido con la iniciativa de responsabilidad corporativa del Pacto Mundial de las Naciones Unidas y sus principios en las áreas de derechos humanos, trabajo, medio ambiente y lucha contra la corrupción.
+            </p>
+        </div>
+
+    </div>
+  )
+}
