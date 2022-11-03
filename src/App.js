dfb6f8a2f3e5534022e7241eed66a53f402d3a38
Daniel Lugo <90514127+daniellfreelancer@users.noreply.github.com>
2022-11-03 01:41:03 -0300

    Added news, Stats + atencion juridica

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logoFundacion.png';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Welcome from './pages/Welcome';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
