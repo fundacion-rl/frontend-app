9be7fb42c798020b4781c8b5f586579cfa05a046
Daniel Lugo <daniellugofreelancer@gmail.com>
2022-11-16 16:22:08 -0300

    Added Bandera + Reseña historica + Valores + Banner + responsive para home + home button

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,16 +3,14 @@ import Bmenu from '../components/Bmenu'
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
 import Sponsors from '../components/Sponsors'
-import UI from '../components/UI'
 
 export default function Layout({children}) {
   return (
     <>
     <div>
     <div className='nav-div'>
-        <NavBar />
         <Bmenu />
-        <UI />
+        <NavBar />
       </div>
       {children}
       <Sponsors/>
--- src/pages/QuienesSomos.jsx
+++ src/pages/QuienesSomos.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import CardsQuienesSomos from '../components/CardsQuienesSomos'
-import Layout from '../layout/Layout'
-import '../styles/QuienesSomos.css'
+import React from "react";
+import BanderaDiv from "../components/BanderaDiv";
+import CardsQuienesSomos from "../components/CardsQuienesSomos";
+import ObjetivoGeneral from "../components/ObjetivoGeneral";
+import Resena from "../components/Resena";
+import Layout from "../layout/Layout";
+import "../styles/QuienesSomos.css";
 
 export default function QuienesSomos() {
-    return (
-        <Layout>
-            <img src='http://drive.google.com/uc?export=view&id=1QlhCOJf6v8nizeO3F6xkV1EBUNAJKuVt' alt='nuestraFundacion' className='img-nuestra-fundacion' />
-            <div className="div-programas">
-            <h2>Quienes Somos</h2>
-        </div>
-            <CardsQuienesSomos/>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <img
+        src="http://drive.google.com/uc?export=view&id=1hiqneTh8ZTGZ0whkXtSc2nptJ-BIsFHS"
+        alt="nuestraFundacion"
+        className="img-nuestra-fundacion"
+      />
+      <div className="div-programas">
+        <h2>Nuestra Fundación</h2>
+      </div>
+      <Resena />
+      <BanderaDiv />
+      <CardsQuienesSomos />
+      <div className="div-programas">
+        <h2>Objetivo General</h2>
+      </div>
+      <ObjetivoGeneral/>
+    </Layout>
+  );
 }
